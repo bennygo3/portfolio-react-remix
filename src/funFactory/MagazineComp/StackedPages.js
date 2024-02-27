b94c443aaf42fe7e497aea6fa11e4a333e0b1863
bennygo3 <3gobenjamin@gmail.com>
2024-02-27 16:52:12 -0600

    making solid progress on the aboutMe section. Toying with getting rid of the Misc section and using the video-s and pictures in the aboutMe section.

--- src/funFactory/MagazineComp/StackedPages.js
+++ src/funFactory/MagazineComp/StackedPages.js
@@ -8,10 +8,16 @@ import Misc from '../../pages/Misc/Misc.js';
 const StackedPages = () => {
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
 
-    const totalPages = 4;
+    const totalPages = 5;
 
     const goToNextSpread = () => {
-        setCurrentPageIndex((prevIndex) => (prevIndex + 2) % totalPages);
+        setCurrentPageIndex((prevIndex) => {
+            if (prevIndex >= totalPages - 2) {
+                return 0;
+            } else {
+                return (prevIndex + 2) % totalPages;
+            }
+        }) 
     };
 
     const goToPreviousSpread = () => {
@@ -24,40 +30,55 @@ const StackedPages = () => {
             case 0:
                 return <div className='aboutMePgOne'>
                     <div>Benjamin A. Gomez</div>
-                    <img className='gCanyon' src={gCanyon} alt='drawing of the grand canyon' />
-                </div>
+                    <div>
+                        <img className='gCanyon' src={gCanyon} alt='drawing of the grand canyon' />
+                    </div>
+                </div>;
             case 1:
                 return <div className='aboutMePgTwo'>
                     <header>
                         <h1 className='pgTwoHeader'>Chapter 1</h1>
                     </header>
                     <div className='horizontalLine'></div>
-                    <div className='aboutMePgTwoParagraph'>
+                    <div className='aboutMePgTwoFirstP'>
                         <img className='youngBe' src={youngMe} alt='Me when I was a child, dressed as a cowboy'></img>
-                        <p>Hello! <br></br> 
+                        <p>Hello! <br></br>
                             My name is Ben Gomez, and I am a Software Engineer.
-                            I was born and raised in Phoenix, Arizona, but now I 
-                            call Austin, Texas
+                            I was born and raised in Phoenix, Arizona, but now I
+                            call Austin, Texas my home
                             <span className='capContainer'>
                                 <img className='capBuilding' src={pease} alt='Capitol building, Austin, Texas. Viewed from Pease Park'></img>
                             </span>
-                            my home.
+                            .
                         </p>
-                        
+
                     </div>
-                    <p>
-                        Shortly after moving to Austin in 2012, I started working at the Fazio Canyons Golf Course at Barton Creek. 
-                        I was chatting with a member about going to school to become a paramedic and he mentioned
+                    <p className='aboutMePgTwoSecondP'>
+                        Shortly after moving to Austin in 2012, I started working at the Fazio Canyons Golf Course at Barton Creek.
+                        One morning, I was chatting with a member about going to school to become a paramedic, and he mentioned
                         that his company developed some of the technology used by 911 call centers. I remember being
-                        impressed that folks had figured out a way to come up with a solution for that need.
-                        I continued on with paramedic school and during a lesson that involved giving medications at certain intervals, 
-                        I had an idea for an application that could help with keeping track of the timing for each medication. During another lesson we learned about the system Austin Travis County EMS uses
+                        perpexled that folks had figured out a way to come up with a solution for that need. My curiosity was piqued!
                     </p>
+
                 </div>;
+            case 2:
+                return <>
+                    <p className='aboutMePgThreeFirstP'>
+                        Another pivotal moment in my transition from EMS to software development occurred during my paramedic training.
+                        We were tasked with administering multiple medications at specific intervals, as needed. For instance, we had to give medication (m) after
+                        2 minutes of chest compressions if the cardiac rhythm displayed (x) rhythm, followed by another dose of medication (m) every 8 minutes if rhythm (x) persisted.
+                        If rhythm (y) was present, we would administer medication (z), followed by more chest compressions and another dose of medication (z) after 4 minutes.
+                        This process was complex and challenging, requiring us to remember a multitude of details while simultaneously keeping track of time. 
+                        At some point in the middle of compressions, I had an idea that could help with keeping track of the timing: an application that notifies the crew during each critical 
+                        interval, providing a reminder that a certain amount of time has passed. 
+                    </p>
+                </>;
             case 3:
-                return<><Misc /></>
+                return <>
+                    <Misc className="aboutMeMisc"/>
+                    </>;
             default:
-                return <div>Default Content</div>;
+                return <div>TBD Content</div>;
         }
     };
 
